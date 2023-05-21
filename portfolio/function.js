f5a4f34678de6e91aef91adc40a3078b9e4d4d41
mattwilfredsalvador <smattwilfred01@gmail.com>
2023-05-21 21:30:39 +0800

    added draft project 1

--- portfolio/function.js
+++ portfolio/function.js
@@ -8,7 +8,7 @@ $header_top.find('a').on('click', function() {
 });
  
 $('#fullpage').fullpage({
-  sectionsColor: ['black', 'black', 'black', '#ff5757', '#ffd03c'],
+  sectionsColor: ['black', 'white', 'white', '#ff5757', '#ffd03c'],
   sectionSelector: '.vertical-scrolling',
   navigation: true,
   slidesNavigation: true,
@@ -30,3 +30,47 @@ $('#fullpage').fullpage({
     }
   },
 });
+
+/** ajfjbdf */
+
+var $body = $('body'),
+  $panel = $('.panel'),
+  $pContent = $('.panel__content'),
+  $img = $('.panel__img-col');
+
+function initTilt() {
+  TweenMax.set([$pContent, $img], { transformStyle: "preserve-3d" });
+
+  $body.mousemove(function(e) {
+    tilt(e.pageX, e.pageY) 
+  });
+};
+
+function tilt(cx, cy) {
+  // var sxPos = cx / $panel.width() * 100 - 100;
+  // var syPos = cy / $panel.height() * 100 - 100;
+  var sxPos = (cx / $body.width()*100 - 50)*2 ;
+  var syPos = (cy / $body.height()*100 - 50)*2;
+  TweenMax.to($pContent, 2, {
+    rotationY: -0.03 * sxPos,
+    rotationX: 0.03 * syPos,
+    transformPerspective: 500,
+    transformOrigin: "center center -400",
+    ease: Expo.easeOut
+  });
+  TweenMax.to($img, 2, {
+    rotationY: -0.03 * sxPos,
+    rotationX: 0.03 * syPos,
+    transformPerspective: 500,
+    transformOrigin: "center center -200",
+    ease: Expo.easeOut
+  });
+}
+
+$body.mouseleave(function() {
+  tilt($body.width()/2, $body.height()/2);
+})
+
+initTilt();
+
+console.clear();
