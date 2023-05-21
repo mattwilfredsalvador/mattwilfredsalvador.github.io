c12bdd738b95cb8117a677461a7a696427345508
mattwilfredsalvador <smattwilfred01@gmail.com>
2023-05-21 11:39:08 +0800

    modifications on the mesh sculpture

--- portfolio/function.js
+++ portfolio/function.js
@@ -8,7 +8,7 @@ $header_top.find('a').on('click', function() {
 });
  
 $('#fullpage').fullpage({
-  sectionsColor: ['black', '#0D0D0E', '#ff8b20', '#ff5757', '#ffd03c'],
+  sectionsColor: ['black', 'black', 'black', '#ff5757', '#ffd03c'],
   sectionSelector: '.vertical-scrolling',
   navigation: true,
   slidesNavigation: true,
